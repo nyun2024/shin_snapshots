128213ae4d3c5c535f04e713f46d2dbab71f61d8
nyun2024 <kws043945@gmail.com>
2025-05-12 15:19:17 +0900

    웹캠 촬영 프레임 테스트

--- src/pages/Snapshot/WebCam.jsx
+++ src/pages/Snapshot/WebCam.jsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from "react";
 import Webcam from "react-webcam";
+import styles from "./WebCam.module.scss";
+import black1 from "@img/frame/black/black1.png";
 
 const WebCam = () => {
   const webcamRef = useRef(null);
@@ -12,17 +14,19 @@ const WebCam = () => {
 
   return (
     <div>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width={320}
-        height={240}
-        videoConstraints={{
-          facingMode: "user",
-        }}
-      />
-      <button onClick={capture}>사진 찍기</button>
+      <div className={styles.webCamFrame}>
+        <img src={black1} />
+        <Webcam
+          className={styles.webCam}
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={{
+            facingMode: "user",
+          }}
+        />
+      </div>
+      <button onClick={capture}>📷 사진 찍기</button>
       {imageSrc && (
         <div>
           <h3>촬영된 사진:</h3>
