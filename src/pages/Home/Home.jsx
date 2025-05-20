ac09f84e2db45a38e7e9a36cc11305f115422d39
nyun2024 <kws043945@gmail.com>
2025-05-20 15:11:45 +0900

    홈 pc, mobile 디자인 반영

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -29,10 +29,40 @@ const Home = () => {
       isHome
     >
       <div className={styles.contentsInner}>
-        <h2 className={styles.title}>
-          <div>Happy Birthday</div>
-          <div>{!isMobile && "Asakura "}Shin</div>
-        </h2>
+        <div className={styles.contentsInfo}>
+          <h2 className={styles.title}>
+            <div>Happy Birthday</div>
+            <div>{!isMobile && "Asakura "}Shin</div>
+          </h2>
+          {!isMobile && (
+            <ul className={styles.shinProfile}>
+              <li>
+                <label>Name</label>
+                <p>Asakura Shin</p>
+              </li>
+              <li>
+                <label>Birthday</label>
+                <p>2000.06.07</p>
+              </li>
+              <li>
+                <label>Height/Weight</label>
+                <p>172 cm/67 kg</p>
+              </li>
+              <li>
+                <label>Affiliation</label>
+                <p>Sakamoto's Store</p>
+              </li>
+              <li>
+                <label>Hobby</label>
+                <p>
+                  Listening to music
+                  <br />
+                  Watching movies
+                </p>
+              </li>
+            </ul>
+          )}
+        </div>
         <div className={styles.homeMainWrap}>
           <img src={starLine} className={styles.starLine} />
           <div className={styles.imgWrap}>
